5e0d4f282c5a9367589066b73263ded6c6a19423
GHC18081998 <106138199+GHC18081998@users.noreply.github.com>
2022-09-24 22:28:04 +0530

    Update rt.tf

--- rt.tf
+++ rt.tf
@@ -1,24 +1,24 @@
 # Creating Route Table
 resource "aws_route_table" "rt" {
-vpc_id = "${aws_vpc.pro-vpc.id}"
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = "${aws_internet_gateway.igw.id}"
-}
-tags = {
-Name = "rt"
-}
+  vpc_id = aws_vpc.pro-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "rt"
+  }
 }
 
 
 # Associating Route Table
 resource "aws_route_table_association" "rt1" {
-subnet_id = "${aws_subnet.web-pub-sub-1.id}"
-route_table_id = "${aws_route_table.rt.id}"
+  subnet_id      = aws_subnet.web-pub-sub-1.id
+  route_table_id = aws_route_table.rt.id
 }
 
 # Associating Route Table
 resource "aws_route_table_association" "rt2" {
-subnet_id = "${aws_subnet.app-pub-sub-1.id}"
-route_table_id = "${aws_route_table.rt.id}"
+  subnet_id      = aws_subnet.app-pub-sub-1.id
+  route_table_id = aws_route_table.rt.id
 }
