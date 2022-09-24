5a0357c25cc0e7cebdf5a58f4d06418780b1c2bb
GHC18081998 <106138199+GHC18081998@users.noreply.github.com>
2022-09-25 01:41:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Creating 1st EC2 instance in Public Subnet
 resource "aws_instance" "web-1" {
-  ami                         = "ami-0f924dc71d44d23e2"
+  ami                         = "ami-026b57f3c383c2eec"
   instance_type               = "t2.micro"
-  key_name                    = "terraform-1"
+  key_name                    = "terraform"
   vpc_security_group_ids      = ["${aws_security_group.web-sg.id}"]
   subnet_id                   = aws_subnet.web-pub-sub-1.id
   associate_public_ip_address = true
@@ -13,9 +13,9 @@ resource "aws_instance" "web-1" {
 }
 # Creating 2nd EC2 instance in Public Subnet
 resource "aws_instance" "web-2" {
-  ami                         = "ami-0f924dc71d44d23e2"
+  ami                         = "ami-026b57f3c383c2eec"
   instance_type               = "t2.micro"
-  key_name                    = "terraform-1"
+  key_name                    = "terraform"
   vpc_security_group_ids      = ["${aws_security_group.web-sg.id}"]
   subnet_id                   = aws_subnet.app-pub-sub-1.id
   associate_public_ip_address = true
@@ -26,9 +26,9 @@ resource "aws_instance" "web-2" {
 }
 # Creating 3rd EC2 instance in Private Subnet
 resource "aws_instance" "db" {
-  ami                    = "ami-0f924dc71d44d23e2"
+  ami                    = "ami-026b57f3c383c2eec"
   instance_type          = "t3.micro"
-  key_name               = "terraform-1"
+  key_name               = "terraform"
   vpc_security_group_ids = ["${aws_security_group.db-sg.id}"]
   subnet_id              = aws_subnet.db-pvt-sub-1.id
   tags = {
