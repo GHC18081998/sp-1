95b90ddeafada19f3a4a0c4e7dd1cc025f57dbbe
GHC18081998 <106138199+GHC18081998@users.noreply.github.com>
2022-09-25 01:44:49 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "web-pub-sub-1" {
   vpc_id                  = aws_vpc.pro-vpc.id
   cidr_block              = "192.168.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
   tags = {
     Name = "web-pub-sub-1"
   }
@@ -13,7 +13,7 @@ resource "aws_subnet" "web-pvt-sub-2" {
   vpc_id                  = aws_vpc.pro-vpc.id
   cidr_block              = "192.168.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1b"
   tags = {
     Name = "web-pub-sub-2"
   }
@@ -23,7 +23,7 @@ resource "aws_subnet" "app-pub-sub-1" {
   vpc_id                  = aws_vpc.pro-vpc.id
   cidr_block              = "192.168.3.0/24"
   map_public_ip_on_launch = false
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1c"
   tags = {
     Name = "app-Sub-1"
   }
@@ -33,7 +33,7 @@ resource "aws_subnet" "app-pvt-sub-2" {
   vpc_id                  = aws_vpc.pro-vpc.id
   cidr_block              = "192.168.4.0/24"
   map_public_ip_on_launch = false
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1d"
   tags = {
     Name = "app-Sub-2"
   }
@@ -42,7 +42,7 @@ resource "aws_subnet" "app-pvt-sub-2" {
 resource "aws_subnet" "db-pvt-sub-1" {
   vpc_id            = aws_vpc.pro-vpc.id
   cidr_block        = "192.168.5.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1e"
   tags = {
     Name = "db-pvt-sub-1"
   }
@@ -52,7 +52,7 @@ resource "aws_subnet" "db-pvt-sub-1" {
 resource "aws_subnet" "db-pvt-sub-2" {
   vpc_id            = aws_vpc.pro-vpc.id
   cidr_block        = "192.168.6.0/24"
-  availability_zone = "us-east-2b"
+  availability_zone = "us-east-1f"
   tags = {
     Name = "db-pvt-sub-2"
   }
